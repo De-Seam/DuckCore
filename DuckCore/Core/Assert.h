caa6843c7f858b4f60ea984c914f928161d92979
Sam van der Hoeven <hoevens@yahoo.com>
2024-10-23 18:23:08 +0200

    Updated

--- DuckCore/Core/Assert.h
+++ DuckCore/Core/Assert.h
@@ -4,16 +4,18 @@
 
 #define BREAKPOINT() __debugbreak()
 
+#define STRINGIFY_MACRO(x) #x
+
 void gLog(const char* inMessage) {}
 
 // Alteratively we could use the same "default" message for both "WITH_MSG" and "NO_MSG" and
 // provide support for custom formatting by concatenating the formatting string instead of having the format inside the default message
-#define HZ_INTERNAL_ASSERT_IMPL(check, msg, ...) { if(!(check)) { gLog(msg); BREAKPOINT(); } }
-#define HZ_INTERNAL_ASSERT_WITH_MSG(check, ...) HZ_INTERNAL_ASSERT_IMPL(type, check, "Assertion failed: {0}", __VA_ARGS__)
-#define HZ_INTERNAL_ASSERT_NO_MSG(check) HZ_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", HZ_STRINGIFY_MACRO(check), std::filesystem::path(__FILE__).filename().string(), __LINE__)
+#define INTERNAL_ASSERT_IMPL(check, msg, ...) { if(!(check)) { gLog(msg); BREAKPOINT(); } }
+#define INTERNAL_ASSERT_WITH_MSG(check, ...) INTERNAL_ASSERT_IMPL(check, "Assertion '%s' failed at %s:%s", STRINGIFY_MACRO(check), std::filesystem::path(__FILE__).filename().string(), __LINE__, __VA_ARGS__)
+#define INTERNAL_ASSERT_NO_MSG(check) INTERNAL_ASSERT_IMPL(check, "Assertion '%s' failed at %s:%s", STRINGIFY_MACRO(check), std::filesystem::path(__FILE__).filename().string(), __LINE__)
 
-#define HZ_INTERNAL_ASSERT_GET_MACRO_NAME(arg1, arg2, macro, ...) macro
-#define HZ_INTERNAL_ASSERT_GET_MACRO(...) EXPAND_MACRO( HZ_INTERNAL_ASSERT_GET_MACRO_NAME(__VA_ARGS__, HZ_INTERNAL_ASSERT_WITH_MSG, HZ_INTERNAL_ASSERT_NO_MSG) )
+#define INTERNAL_ASSERT_GET_MACRO_NAME(arg1, arg2, macro, ...) macro
+#define INTERNAL_ASSERT_GET_MACRO(...) EXPAND_MACRO( INTERNAL_ASSERT_GET_MACRO_NAME(__VA_ARGS__, INTERNAL_ASSERT_WITH_MSG, INTERNAL_ASSERT_NO_MSG) )
 
 // Currently accepts at least the condition and one additional parameter (the message) being optional
-#define gAssert(...) EXPAND_MACRO( HZ_INTERNAL_ASSERT_GET_MACRO(__VA_ARGS__)(_, __VA_ARGS__) )
+#define gAssert(...) EXPAND_MACRO( INTERNAL_ASSERT_GET_MACRO(__VA_ARGS__)(__VA_ARGS__) )
