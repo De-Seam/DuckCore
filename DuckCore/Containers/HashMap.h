184c73145cfca1be306c45732fa862adb31adfec
Sam van der Hoeven <hoevens@yahoo.com>
2024-10-24 07:47:18 +0200

    Added HashMap

---
+++ DuckCore/Containers/HashMap.h
@@ -0,0 +1,60 @@
+#pragma once
+// Core includes
+#include <DuckCore/Config.h>
+
+// External includes
+#include <External/phmap/phmap.h>
+
+namespace DC
+{
+template<typename taKey, typename taValue>
+class HashMap
+{
+public:
+	HashMap() = default;
+
+	taValue& operator[](const taKey& inKey) { return mMap[inKey]; }
+	const taValue& operator[](const taKey& inKey) const { return mMap.at(inKey); }
+
+	bool Remove(const taKey& inKey) { return mMap.erase(inKey); }
+
+	bool IsEmpty() const { return mMap.empty(); }
+	bool Contains(const taKey& inKey) const { return mMap.contains(inKey); }
+
+	taValue* Find(const taKey& inKey);
+	const taValue* Find(const taKey& inKey) const;
+
+	template<typename taLambda>
+	void ForEach(taLambda&& inLambda);
+
+private:
+	phmap::flat_hash_map<taKey, taValue> mMap;
+};
+
+template <typename taKey, typename taValue>
+taValue* HashMap<taKey, taValue>::Find(const taKey& inKey)
+{
+	auto it = mMap.find(inKey);
+	if (it != mMap.end())
+		return &it->second;
+	return nullptr;
+}
+
+template <typename taKey, typename taValue>
+const taValue* HashMap<taKey, taValue>::Find(const taKey& inKey) const
+{
+	auto it = mMap.find(inKey);
+	if (it != mMap.end())
+		return &it->second;
+	return nullptr;
+}
+
+template<typename taKey, typename taValue>
+template<typename taLambda>
+void HashMap<taKey, taValue>::ForEach(taLambda&& inLambda)
+{
+	for (auto& [key, value] : mMap)
+		inLambda(key, value);
+}
+
+}
