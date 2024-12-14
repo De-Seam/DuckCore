e1c6caa2f2fa83c5a60a33e7b8c0537501eec8ea
Sam van der Hoeven <hoevens@yahoo.com>
2024-12-14 22:39:21 +0100

    Update NoCopy.h

--- DuckCore/Utilities/NoCopy.h
+++ DuckCore/Utilities/NoCopy.h
@@ -1,18 +1,22 @@
 #pragma once
+
+namespace DC
+{
 class NoCopy
 {
 public:
 	NoCopy() = default;
 
 	NoCopy(const NoCopy&) = delete;
-    NoCopy& operator=(const NoCopy&) = delete;
+	NoCopy& operator=(const NoCopy&) = delete;
 };
 
 class NoMove
 {
 public:
-    NoMove() = default;
+	NoMove() = default;
 
-    NoMove(NoMove&&) = delete;
-    NoMove& operator=(NoMove&&) = delete; 
+	NoMove(NoMove&&) = delete;
+	NoMove& operator=(NoMove&&) = delete; 
 };
+}
