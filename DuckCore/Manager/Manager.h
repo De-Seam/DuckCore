8dedc3872ef94ba353bbc9d17a9a79b185960039
Sam van der Hoeven <hoevens@yahoo.com>
2025-02-11 21:55:43 +0100

    Added Manager

---
+++ DuckCore/Manager/Manager.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <DuckCore/RTTI/RTTIClass.h>
+
+namespace DC
+{
+	class Manager : public RTTIClass
+	{
+		RTTI_CLASS(Manager, RTTIClass)
+	public:
+
+	private:
+	};
+}
---
+++ DuckCore/Manager/Managers.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <DuckCore/Containers/HashMap.h>
+#include <DuckCore/Containers/UniquePtr.h>
+#include <DuckCore/Manager/Manager.h>
+
+namespace DC
+{
+class Managers
+{
+public:
+	template<typename taManagerType>
+	static void sAdd(UniquePtr<taManagerType> inManager);
+
+	template<typename taManagerType>
+	static taManagerType& sGet();
+
+	template<typename taManagerType>
+	static taManagerType* sFind();
+
+private:
+	Managers() = default;
+
+	static HashMap<const RTTI*, UniquePtr<Manager>> sManagers;
+};
+
+template<typename taManagerType>
+void Managers::sAdd(UniquePtr<taManagerType> inManager) 
+{
+	const RTTI* rtti = &taManagerType::sGetRTTI();
+	gAssert(!sManagers.Contains(rtti));
+
+	sManagers[rtti] = gMove(inManager);
+}
+
+template<typename taManagerType>
+taManagerType& Managers::sGet() 
+{
+	const RTTI* rtti = &taManagerType::sGetRTTI();
+	gAssert(sManagers.Contains(rtti));
+
+	return sManagers.At(rtti);
+}
+
+template<typename taManagerType>
+taManagerType* Managers::sFind() 
+{
+	const RTTI* rtti = &taManagerType::sGetRTTI();
+	if (sManagers.Contains(rtti))
+		return &sManagers.At(rtti);
+	return nullptr;
+}
+}
