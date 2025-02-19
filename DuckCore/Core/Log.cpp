5be0bf9f59e64dc30d4323e1eec1932cd1b53306
Sam van der Hoeven <hoevens@yahoo.com>
2025-02-19 17:14:49 +0100

    Removed LogManager

--- DuckCore/Core/Log.cpp
+++ DuckCore/Core/Log.cpp
@@ -1,13 +1,12 @@
 #include <DuckCore/Core/Log.h>
 
+#include <DuckCore/Containers/File/TextFile.h>
 #include <DuckCore/Containers/Array.h>
 #include <DuckCore/Containers/HashMap.h>
 #include <DuckCore/Containers/String.h>
 #include <DuckCore/Core/Assert.h>
-#include <DuckCore/Core/LogManager.h>
 #include <DuckCore/Manager/Managers.h>
 #include <DuckCore/Threads/ScopedMutex.h>
-
 #include <cstdio>
 
 namespace DC
