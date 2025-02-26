31419d6923c8ecad3f4a2bd5f80d5b7a774a25c4
Sam van der Hoeven <hoevens@yahoo.com>
2025-02-26 07:09:10 +0100

    Fixed compile errors

--- DuckCore/Math/HelperFunctions.h
+++ DuckCore/Math/HelperFunctions.h
@@ -5,14 +5,14 @@ namespace DC
 static constexpr float cPiF = 3.14159265358979323846f;
 static constexpr double cPi = 3.14159265358979323846;
 
-inline float DegreesToRadians(float aDegrees);
-inline double DegreesToRadians(double aDegrees);
+float DegreesToRadians(float aDegrees);
+double DegreesToRadians(double aDegrees);
 
-inline float RadiansToDegrees(float aRadians);
-inline double RadiansToDegrees(double aRadians);
+float RadiansToDegrees(float aRadians);
+double RadiansToDegrees(double aRadians);
 
-inline float Sqrt(float aValue);
-inline double Sqrt(double aValue);
+float Sqrt(float aValue);
+double Sqrt(double aValue);
 
 template<typename taType>
 inline taType Lerp(taType aFrom, taType aTo, taType aT)
