1003c007646a77ed6a1e0cf30c5508d6b6f7531e
Sam van der Hoeven <hoevens@yahoo.com>
2025-02-26 17:39:10 +0100

    Update EventManager.h

--- DuckCore/Events/EventManager.h
+++ DuckCore/Events/EventManager.h
@@ -26,16 +26,15 @@ class EventHandle final : public EventHandleBase
 public:
 	virtual ~EventHandle() override;
 	EventHandle() = delete;
-
-protected:
-	virtual void Call(Event& ioEvent) override { mOnEventFunction(*gStaticCast<taEventType*>(&ioEvent)); }
-
-private:
 	EventHandle(std::function<void(taEventType&)> inOnEventFunction, EventManager& inEventManager) :
 		mOnEventFunction(Move(inOnEventFunction)),
 		mEventManager(&inEventManager)
 	{}
 
+protected:
+	virtual void Call(Event& ioEvent) override { mOnEventFunction(*gStaticCast<taEventType*>(&ioEvent)); }
+
+private:
 	std::function<void(taEventType&)> mOnEventFunction;
 	EventManager* mEventManager = nullptr;
 
@@ -46,7 +45,7 @@ class EventManager : public Manager
 {
 	MANAGER_BASE_CLASS(EventManager)
 public:
-	template<typename taEventType>
+	template<typename taEventType> [[nodiscard]]
 	UniquePtr<EventHandle<taEventType>> AddEventListener(std::function<void(taEventType&)> inFunction);
 
 	template<typename taEventType>
@@ -57,6 +56,7 @@ private:
 	void UnregisterEventHandle(const EventHandle<taEventType>& inEventHandle);
 
 	HashMap<const RTTI*, Array<EventHandleBase*>> mEventCallbacks;
+	IF_ASSERTS(Array<const RTTI*> mCurrentlyBroadcastingEvents;) // Asserts-only, array of events we are currently broadcasting using SendEvent.
 
 	template<typename taEventType>
 	friend class EventHandle;
@@ -72,10 +72,9 @@ template<typename taEventType>
 UniquePtr<EventHandle<taEventType>> EventManager::AddEventListener(std::function<void(taEventType&)> inFunction)
 {
 	gAssert(gIsMainThread());
+	gAssert(!mCurrentlyBroadcastingEvents.Contains(&taEventType::sGetRTTI()), "Cant register event listeners while broadcasting that specific event.");
 
-	UniquePtr<EventHandle<taEventType>> handle = MakeUnique<EventHandle<taEventType>>();
-	handle->mOnEventFunction = inFunction;
-	handle->mEventManager = this;
+	UniquePtr<EventHandle<taEventType>> handle = MakeUnique<EventHandle<taEventType>>(inFunction, *this);
 
 	const RTTI* rtti = &taEventType::sGetRTTI();
 	mEventCallbacks[rtti].Add(handle);
@@ -89,16 +88,23 @@ void EventManager::SendEvent(taEventType& ioEvent)
 	gAssert(gIsMainThread());
 
 	const RTTI* rtti = &taEventType::sGetRTTI();
+
+	gAssert(!mCurrentlyBroadcastingEvents.Contains(rtti), "Cant send event while broadcasting that same event.");
+	IF_ASSERTS(mCurrentlyBroadcastingEvents.Add(rtti);)
+
 	Array<EventHandleBase*>& event_callbacks = mEventCallbacks[rtti];
 
 	for (EventHandleBase* handle : event_callbacks)
 		handle->Call(ioEvent);
+
+	IF_ASSERTS(mCurrentlyBroadcastingEvents.RemoveFirstIf([rtti](const RTTI* aRTTI) { return rtti == aRTTI; });)
 }
 
 template<typename taEventType>
 void EventManager::UnregisterEventHandle(const EventHandle<taEventType>& inEventHandle)
 {
 	gAssert(gIsMainThread());
+	gAssert(!mCurrentlyBroadcastingEvents.Contains(&taEventType::sGetRTTI()), "Cant unregister event listeners while broadcasting that specific event.");
 
 	const RTTI* rtti = &taEventType::sGetRTTI();
 	Array<EventHandleBase*>& event_callbacks = mEventCallbacks[rtti];
