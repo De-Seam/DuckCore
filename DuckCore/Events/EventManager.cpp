0675a424219ec288618765828afab53d04ff86fc
Sam van der Hoeven <36552867+De-Seam@users.noreply.github.com>
2025-05-06 21:59:47 +0200

    Fixed errors

--- DuckCore/Events/EventManager.cpp
+++ DuckCore/Events/EventManager.cpp
@@ -6,7 +6,7 @@ void EventManager::UnregisterEventHandle(const EventHandle& inEventHandle)
 {
 	ScopedMutexRecursiveLock lock(mMutex);
 
-	gAssert(!mCurrentlyBroadcastingEvents.Contains(&taEventType::sGetRTTI()), "Cant unregister event listeners while broadcasting that specific event.");
+	gAssert(!mCurrentlyBroadcastingEvents.Contains(&inEventHandle.mEventRTTI), "Cant unregister event listeners while broadcasting that specific event.");
 
 	const RTTI* rtti = &inEventHandle.mEventRTTI;
 	Array<EventHandle*>& event_callbacks = mEventCallbacks[rtti];
